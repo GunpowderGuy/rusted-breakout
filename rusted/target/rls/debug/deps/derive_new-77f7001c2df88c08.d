83d6624d143caac8eac5c2548667466eea2f458b
Diego Rosario <drosario@localhost.localdomain>
2018-03-20 21:16:40 -0500

    bouncing implemented off the now wide pc

---
+++ rusted/target/rls/debug/deps/derive_new-77f7001c2df88c08.d
@@ -0,0 +1,5 @@
+/home/drosario/Documents/rusted-breakout/rusted/target/rls/debug/deps/libderive_new-77f7001c2df88c08.so: /home/drosario/.cargo/registry/src/github.com-1ecc6299db9ec823/derive-new-0.5.1/src/lib.rs
+
+/home/drosario/Documents/rusted-breakout/rusted/target/rls/debug/deps/derive_new-77f7001c2df88c08.d: /home/drosario/.cargo/registry/src/github.com-1ecc6299db9ec823/derive-new-0.5.1/src/lib.rs
+
+/home/drosario/.cargo/registry/src/github.com-1ecc6299db9ec823/derive-new-0.5.1/src/lib.rs:
