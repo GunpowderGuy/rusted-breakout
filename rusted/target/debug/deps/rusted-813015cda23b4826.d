ff8e3f70d77b02d36b15c2bd50a48d556dbe3570
Diego Rosario <diegorosario@localhost.localdomain>
2018-08-09 08:53:22 -0500

    now using the zcomponents vesion from rust crates

---
+++ rusted/target/debug/deps/rusted-813015cda23b4826.d
@@ -0,0 +1,7 @@
+/home/diegorosario/Documentos/rusted-breakout/rusted/target/debug/deps/rusted-813015cda23b4826: src/main.rs src/mundo.rs src/screen.rs
+
+/home/diegorosario/Documentos/rusted-breakout/rusted/target/debug/deps/rusted-813015cda23b4826.d: src/main.rs src/mundo.rs src/screen.rs
+
+src/main.rs:
+src/mundo.rs:
+src/screen.rs:
---
+++ rusted/target/debug/deps/zcomponents-65e59705e639df61.d
@@ -0,0 +1,5 @@
+/home/diegorosario/Documentos/rusted-breakout/rusted/target/debug/deps/libzcomponents-65e59705e639df61.rlib: /home/diegorosario/.cargo/registry/src/github.com-1ecc6299db9ec823/zcomponents-0.1.1/src/lib.rs
+
+/home/diegorosario/Documentos/rusted-breakout/rusted/target/debug/deps/zcomponents-65e59705e639df61.d: /home/diegorosario/.cargo/registry/src/github.com-1ecc6299db9ec823/zcomponents-0.1.1/src/lib.rs
+
+/home/diegorosario/.cargo/registry/src/github.com-1ecc6299db9ec823/zcomponents-0.1.1/src/lib.rs:
